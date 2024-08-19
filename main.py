7f48812139b610744045741dfb5ab1f815d9a8e5
Thierry-DV <thierryuchoa7@gmail.com>
2024-08-19 15:01:05 -0300

    Criando Interface

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk, font, messagebox
+from tkinter import PhotoImage
+
+
+#Criar Interface do APP
+interface = tk.Tk()
+interface.title("Meu Gerenciador de Tarefas")
+interface.configure(bg="#000000")
+interface.geometry("550x650")
+
+interface.mainloop()
